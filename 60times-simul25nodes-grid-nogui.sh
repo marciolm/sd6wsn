874c058181780184f71123f1cb91ee9ded402841
Instant Contiki <marcio.miguel@gmail.com>
2018-04-04 19:55:51 -0300

    new test scripts

---
+++ 60times-simul25nodes-grid-nogui.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+for ((i=1;i<=56;i++));
+do 
+pkill -9 tail
+dt=$(date '+%d%m%Y-%H%M%S')
+simul=simul-sd6wsn-25motes.csc
+dtseed=$(date '+%H%M%S');
+cat $simul | sed -e "s/<randomseed>123456/<randomseed>$dtseed/g" > $dtseed.csc
+rm /COOJA.testlog
+java -mx512m -jar /home1/user/contiki/tools/cooja/dist/cooja.jar -nogui="/home1/user/contiki/examples/sd6wsn/$dtseed.csc" -contiki="/home1/user/contiki" &
+sleep 30
+tail -f ./COOJA.testlog >> ./COOJA.testlog.$dt.txt &
+sleep 240
+node ./scripts/sd6wsn-25nodes-app-v2.js >> ./COOJA.testlog.$dt.txt
+rm $dtseed.csc
+sleep 1200
+done
